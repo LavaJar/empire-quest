cde9f815206ef1d5362bf23b81628455393989db
Lava Jar <jephopz@gmail.com>
2026-08-09 19:21:08 -0400

    Export Battle Theater RenderBlock bridge

--- src/renderblocks/index.ts
+++ src/renderblocks/index.ts
@@ -3,4 +3,6 @@ export * from './RenderBlockRegistry';
 export * from './CanvasRenderBlockRenderer';
 export * from './RenderBlockEngine';
 export * from './RenderBlockState';
+export * from './BattleRenderBlockAdapter';
+export * from './NorthwellBattleBenchmark';
 export * from './prototypeManifest';
